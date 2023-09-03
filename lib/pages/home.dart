0d74989a525bec9a67f64d3eb6032fd1080cea1e
jhih_yu <thomas.JY.Lin@gmail.com>
2023-09-03 16:14:01 +0800

    [New] Add version display

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:tteewwkkrr/widget/scaffold_messenger.dart';
@@ -67,58 +68,83 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 600),
-          height: MediaQuery.of(context).size.height -
-              AppBar().preferredSize.height -
-              MediaQuery.of(context).padding.top -
-              MediaQuery.of(context).padding.bottom,
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '我想要...',
-                  style: Theme.of(context).textTheme.headlineMedium,
+                constraints: const BoxConstraints(
+                  maxWidth: 600,
                 ),
-              ),
-              CustomCard(
+                height: MediaQuery.of(context).size.height -
+                    AppBar().preferredSize.height -
+                    MediaQuery.of(context).padding.top -
+                    MediaQuery.of(context).padding.bottom,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    const SizedBox(
-                      height: 10,
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        '我想要...',
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
                     ),
-                    ListTile(
-                      title: const Text('搜尋課程評論'),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/comment');
-                      },
-                      trailing: const Icon(Icons.chevron_right),
-                    ),
-                    // TODO: 完成搜尋加簽資訊
-                    ListTile(
-                      title: const Text('搜尋加簽資訊'),
-                      subtitle: const Text('開發中'),
-                      onTap: () {
-                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                        CustomScaffoldMessenger.showMessageSnackBar(
-                            context,
-                            _tapTextList[_tapCount >= _tapTextList.length
-                                ? _tapTextList.length - 1
-                                : _tapCount]);
-                        setState(() {
-                          _tapCount++;
-                        });
-                      },
-                      trailing: const Icon(Icons.chevron_right),
-                    ),
-                    const SizedBox(
-                      height: 10,
+                    CustomCard(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          ListTile(
+                            title: const Text('搜尋課程評論'),
+                            onTap: () {
+                              Navigator.pushNamed(context, '/comment');
+                            },
+                            trailing: const Icon(Icons.chevron_right),
+                          ),
+                          // TODO: 完成搜尋加簽資訊
+                          ListTile(
+                            title: const Text('搜尋加簽資訊'),
+                            subtitle: const Text('開發中'),
+                            onTap: () {
+                              ScaffoldMessenger.of(context)
+                                  .hideCurrentSnackBar();
+                              CustomScaffoldMessenger.showMessageSnackBar(
+                                  context,
+                                  _tapTextList[_tapCount >= _tapTextList.length
+                                      ? _tapTextList.length - 1
+                                      : _tapCount]);
+                              setState(() {
+                                _tapCount++;
+                              });
+                            },
+                            trailing: const Icon(Icons.chevron_right),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
                     ),
                   ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: FutureBuilder(
+                  future: PackageInfo.fromPlatform(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<PackageInfo> snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(
+                          '${snapshot.data!.version}+${snapshot.data!.buildNumber}');
+                    } else {
+                      return const Text('Loading...');
+                    }
+                  },
                 ),
               ),
             ],
